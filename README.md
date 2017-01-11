c5dd026dfe815bdbf991735b8aedfd53c418d8a1
mac <mac.c@360integrity.com>
2017-01-11 13:39:50 +0000

    Added Readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# pause-fire-stick-with-dash
+Small node script to pause a fire tv or stick when pressed, using adb to send keycode 85 - play/pause.
+
+#Requirements
+#Debian based distro
+sudo apt-get install libpcap-dev
+
+#windows
+https://www.winpcap.org/
+
+#Then for all arch
+npm install
+
+#Note
+This repo includes the adb binary for windows only.
+If you have the Android SDK installed or the adb tools bundle and your PATH variable is set to include links to adb then you
+should not need to use a binary in the project root.
--- app.js
+++ app.js
@@ -8,66 +8,67 @@ var errors = [],
     ip = '192.168.0.102',
     result = ''
 
-function killServer(callBack){
-  var kill = exec('adb kill-server');
-      kill.stdout.on('data', function(data) {
-          result+='Kill server result \n'
-          result += data
-      })
-      kill.on('close', function() {
-        if(result==''){
-          result+='Kill server done \n'
+function killServer(callBack) {
+    var kill = exec('adb kill-server');
+    kill.stdout.on('data', function(data) {
+        result += 'Kill server result \n'
+        result += data
+    })
+    kill.on('close', function() {
+        if (result == '') {
+            result += 'Kill server done \n'
         }
         callBack(null)
-      })
+    })
 }
 
-function tcpip(callBack){
-  var tcpip = exec('adb tcpip 5555')
-      tcpip.stdout.on('data', function(data) {
-          result+='tcpip result \n'
-          result += data
-      })
-      tcpip.stdout.on('close',function(data){
+function tcpip(callBack) {
+    var tcpip = exec('adb tcpip 5555')
+    tcpip.stdout.on('data', function(data) {
+        result += 'tcpip result \n'
+        result += data
+    })
+    tcpip.stdout.on('close', function(data) {
         callBack(null)
-      })
+    })
 }
 
-function connect(callBack){
-  var connectIP = exec('adb connect '+ip+':5555')
-      connectIP.stdout.on('data', function(data) {
-        result+='connect result \n'
-          result += data
-      })
-      connectIP.stdout.on('close',function(data){
+function connect(callBack) {
+    var connectIP = exec('adb connect ' + ip + ':5555')
+    connectIP.stdout.on('data', function(data) {
+        result += 'connect result \n'
+        result += data
+    })
+    connectIP.stdout.on('close', function(data) {
         callBack(null)
-      })
+    })
 }
 
-function sendKey85(callBack){
-  setTimeout(function(){
-    var sendKeys = exec('adb shell input keyevent 85')
-        sendKeys.stdout.on('close',function(data){
-          callBack(null,result)
+function sendKey85(callBack) {
+    setTimeout(function() {
+        var sendKeys = exec('adb shell input keyevent 85')
+        sendKeys.stdout.on('close', function(data) {
+            callBack(null, result)
         })
-  },2000)
+    }, 1000)
 }
 
-function controller(){
-  var devices = exec('adb devices')
-      devices.stdout.on('data',function(data){
-        if(data.search(ip)==-1){
-          waterfall([killServer,tcpip,connect,sendKey85],function(err,result){
-            console.log(result)
-          })
-        }else{
-          var sendKeys = exec('adb shell input keyevent 85')
-          sendKeys.stdout.on('close',function(data){
-            console.log('already connected sendKeys done')
-          })
+function controller() {
+    var devices = exec('adb devices')
+    devices.stdout.on('data', function(data) {
+        if (data.search(ip) == -1) {
+            waterfall([killServer, tcpip, connect, sendKey85], function(err, result) {
+                console.log(result)
+            })
+        } else {
+            var sendKeys = exec('adb shell input keyevent 85')
+            sendKeys.stdout.on('close', function(data) {
+                console.log('already connected sendKeys done')
+            })
         }
-      })
+    })
 }
+
 controller()
 //dash.on("detected", function (){
 //    controller()
